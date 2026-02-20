4d5defe5fa9b5d263fe9b98fa8bf710be373a5f1
Ben Aw Yong <herbalvegetable@gmail.com>
2026-02-20 23:25:30 +0800

    fastapi server debugging changes

--- server/fastapi/model_api.py
+++ server/fastapi/model_api.py
@@ -7,8 +7,13 @@ app = FastAPI()
 class RuleRequest(BaseModel):
     rules: str
 
+@app.get("/")
+async def index():
+    return {"message": "Hello World"}
+
 @app.post("/generate-diagram")
 async def generate(req: RuleRequest):
+    print("Generate diagram")
     result = generate_diagram(req.rules)
     return {"output": result}
 
